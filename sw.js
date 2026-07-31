588e5c5087fcc604f1488d4bd9dfc265605c65c0
guylatronche-racooon <guy_latronche@yahoo.fr>
2026-07-31 22:23:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "acjd-flight-tools-static-v2";
+const CACHE = "acjd-flight-tools-static-v4-super-guepard";
 const CORE = ["/", "/index.html"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
